f05b0e29456f2c496eb91002577ab6a5009a8fad
CrowCrowzard <crowizard@gmail.com>
2016-11-06 23:05:07 +0900

    language processing 20

---
+++ language_processing/20/20.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+import gzip
+import json
+
+with gzip.open('jawiki-country.json.gz', 'rt') as file:
+    for line in file:
+        data = json.loads(line)
+        if data['title'] == 'イギリス':
+            print (data['text'])
+
