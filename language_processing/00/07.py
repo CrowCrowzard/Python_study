df299e681b3342d71c075335dfb82c58cd1d08ee
CrowCrowzard <crowizard@gmail.com>
2016-11-10 17:07:06 +0900

    language processing 7

---
+++ language_processing/00/07.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+def get_info(x, y, z):
+    return '%d時の%sは%.1f' % (x, y, z)
+
+def main():
+    x = 12
+    y = '気温'
+    z = 22.4
+    print (get_info(x, y, z))
+
+if __name__ == '__main__':
+    main()
+
