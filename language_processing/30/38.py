a2e0f8adf8219b78c32e4aa4ff71ab511510182d
CrowCrowzard <crowizard@gmail.com>
2016-11-19 23:42:34 +0900

    language processing 38

---
+++ language_processing/30/38.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+import MeCab
+from collections import Counter
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+input_file  = 'neko.txt'
+output_file = 'neko.txt.mecab'
+
+def parse_neko():
+    with open(input_file, 'r') as in_f, open(output_file, 'w') as out_f:
+        mecab = MeCab.Tagger()
+        out_f.write(mecab.parse(in_f.read()))
+
+def neko_lines():
+    with open(output_file, 'r') as out_f:
+        morphemes = []
+        for line in out_f:
+            cols = line.split('\t')
+            if (len(cols) < 2):
+                raise StopIteration
+            res_cols = cols[1].split(',')
+
+            morpheme = {
+                'surface': cols[0],
+                'base': res_cols[6],
+                'pos': res_cols[0],
+                'pos1': res_cols[1]
+            }
+            morphemes.append(morpheme)
+
+            if res_cols[1] == '句点':
+                yield morphemes
+                morphemes = []
+
+def main():
+    word_counter = Counter()
+    for line in neko_lines():
+        word_counter.update([morpheme['surface'] for morpheme in line])
+
+    size = 10
+    list_word = word_counter.most_common(size)
+    print (list_word)
+
+    list_zipped = list(zip(*list_word))
+    words = list_word[0]
+    counts = list_word[1]
+
+    fp = FontProperties(
+        fname ='./font/ipag.ttf'
+    )
+
+    plt.hist(
+        counts,
+        bins=20,
+        range=(1,20)
+    )
+
+    plt.xlim(xmin=1, xmax=20)
+
+    plt.title("38. ヒストグラム", fontproperties=fp)
+    plt.xlabel('出現頻度', fontproperties=fp)
+    plt.ylabel('単語の種類数', fontproperties=fp)
+
+    plt.grid(axis='y')
+    plt.show()
+
+if __name__ == '__main__':
+    #parse_neko()
+    main()
+
