1013c7b28f6b1beb36d15d8b5dc7d51f5c4d111a
CrowCrowzard <crowizard@gmail.com>
2016-12-16 15:56:29 +0900

    gevent Groups and Pools

---
+++ gevent/group_and_pool.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+import gevent
+from gevent.pool import Group
+
+def talk(msg):
+    for i in range(3):
+        print(msg)
+
+def main():
+    g1 = gevent.spawn(talk, 'bar')
+    g2 = gevent.spawn(talk, 'foo')
+    g3 = gevent.spawn(talk, 'fizz')
+
+    group = Group()
+    group.add(g1)
+    group.add(g2)
+    group.join()
+
+    group.add(g3)
+    group.join()
+
+if __name__ == '__main__':
+    main()
+
+
