bb05d10923595608ae5debd1ed192aa2591e5c31
CrowCrowzard <crowizard@gmail.com>
2016-12-24 12:45:20 +0900

    language prrocessing 50

--- language_processing/50/50.py
+++ language_processing/50/50.py
@@ -1,17 +1,50 @@
 #!/usr/bin/env python3
 # coding: UTF-8
 
-delimiter = ".;:?!"
+import re
 
-with open('nlp.txt', 'r') as file:
-    sentence = ""
-    for line in file:
-        for c in line:
-            if c.isupper():
-                if (len(sentence) > 2 and sentence[-1] == ' ' and (sentence[-2] in delimiter)):
-                    print (sentence)
-                    sentence = ""
-            else:
-                sentence += c
-    print (sentence)
+fname = 'nlp.txt'
 
+def nlp_lines():
+    '''
+    nlp.txtを1文ずつ読み込むジェネレータ
+    nlp.txtを順次読み込んで1文ずつ返す
+
+    返り値:
+    1文の文字列
+    '''
+    with open(fname, 'r') as lines:
+        # 文字切り出しの正規表現コンパイル
+        pattern = re.compile(r'''
+            (
+                ^               # 行頭
+                .*?             # 任意のn文字、最小マッチ
+                [\.|\;|\:\?\!]  # . or ; or : or ? or !
+            )
+            \s                  # 空白文字
+            (
+                [A-Z].*         # 英大文字移行(＝次の文以降)
+            )
+        ''', re.MULTILINE + re.VERBOSE + re.DOTALL)
+
+        for line in lines:
+            line = line.strip() # 前後の空白文字除去
+            while len(line) > 0:
+                # 行から1文字を取得
+                match = pattern.match(line)
+                if match:
+                    # 切り出した文を返す
+                    yield match.group(1)    # 先頭の文
+                    line = match.group(2)   # 次の文以降
+                else:
+                    # 区切りがないので、最後までが1文
+                    yield line
+                    line = ''
+
+def main():
+    for line in nlp_lines():
+        print (line)
+
+
+if __name__ == '__main__':
+    main()
